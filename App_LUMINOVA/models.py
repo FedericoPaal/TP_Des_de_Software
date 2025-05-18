42d59bb4226a43cc7fc26135c2702baffba4b3aa
Dario <dgimenez.developer@gmail.com>
2025-05-18 00:22:41 -0300

    Se agregan modelos para Descripción de roles y auditoría de accesos

--- App_LUMINOVA/models.py
+++ App_LUMINOVA/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ProductoTerminado(models.Model):
@@ -73,3 +74,21 @@ class Factura(models.Model):
 
     def __str__(self):
         return self.numero_factura
+
+
+# Creacion del modelo RolDescripcion
+from django.db import models
+from django.contrib.auth.models import Group
+
+class RolDescripcion(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name='descripcion_extendida')
+    descripcion = models.TextField("Descripción del rol", blank=True)
+
+    def __str__(self):
+        return f"{self.group.name}"
+
+# Creacion del modelo AuditoriaAcceso
+class AuditoriaAcceso(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    accion = models.CharField(max_length=100)
+    fecha_hora = models.DateTimeField(auto_now_add=True)
