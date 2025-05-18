22d14376e2d64ccd7d82ae91f1067290ec4fc57e
Dario <dgimenez.developer@gmail.com>
2025-05-18 00:26:02 -0300

    Se realizan cambios para el registro de app, configuración de roles y auditoría

--- App_LUMINOVA/admin.py
+++ App_LUMINOVA/admin.py
@@ -9,3 +9,9 @@ admin.site.register(Usuario)
 admin.site.register(Proveedor)
 admin.site.register(Cliente)
 admin.site.register(Factura)
+
+# Registro del modelo RolDescripcion en el admin de Django
+from django.contrib import admin
+from .models import RolDescripcion
+
+admin.site.register(RolDescripcion)
